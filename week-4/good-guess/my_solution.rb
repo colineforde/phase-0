47ff95f9340b12586ef845c8d62bc3993d9831d0
Coline Forde <coline.forde@gmail.com>
2015-12-22 13:52:14 -0800

    add response to good guess solution and update errors file

---
+++ week-4/good-guess/my_solution.rb
@@ -0,0 +1,15 @@
+# Good Guess
+
+# I worked on this challenge by myself.
+
+# Create a method called good_guess? that accepts an integer value and returns true if the input is equal to 42 and false otherwise.
+
+# Your Solution Below
+
+def good_guess?(num)
+	if num == 42
+		return true
+	else
+		return false
+	end 
+end
